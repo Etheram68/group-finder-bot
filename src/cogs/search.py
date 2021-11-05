833937a9bb1f29314d5501a30f85fa85af1b6bbf
Etheram68 <francois681927@hotmail.fr>
2021-11-05 11:25:58 +0100

    First commit, add struct base, and command for init

---
+++ src/cogs/search.py
@@ -0,0 +1,69 @@
+import discord
+from discord import channel
+from discord import client
+from discord.ext import commands
+import asyncio
+from src.dao.classifier import DaoFactory
+
+
+class Search(commands.Cog):
+	def __init__(self, bot, db):
+		print("Init Cog")
+		self.db = db
+		self.bot = bot
+
+
+	@commands.command()
+	async def help(self, ctx):
+		embed = discord.Embed(title="Help", description="",color=0x7289da)
+		await ctx.channel.send(embed=embed)
+
+
+	@commands.command(name="del")
+	async def delete(self, ctx, number: int):
+		messages = await ctx.channel.history(limit=number+1).flatten()
+		for each_message in messages:
+			await each_message.delete()
+
+
+	@commands.command(name="setup")
+	@commands.has_permissions(administrator=True)
+	async def setup(self, ctx):
+		guildID = ctx.guild.id
+		id = ctx.author.id
+		def check(m):
+			return m.author.id == ctx.author.id
+		await ctx.channel.send("** You have 60 second to answer each Questions! **")
+		await ctx.channel.send("** Enter id of category for create guild channel? **")
+		try:
+			cat_id = await self.bot.wait_for('message', check=check, timeout=60.0)
+			cat_guild = discord.utils.get(ctx.guild.categories, id=int(cat_id.content))
+			if not cat_guild:
+				raise Exception("Guild id not found")
+		except asyncio.TimeoutError:
+			await ctx.channel.send('Took too long to answer!')
+		except:
+			await ctx.channel.send("Error: You didn't enter the id properly.\nUse `!setup` again!")
+		else:
+			await ctx.channel.send("** Enter the name of the text channel: (e.g Find player)**")
+			try:
+				channel = await self.bot.wait_for('message', check=check, timeout=60.0)
+			except asyncio.TimeoutError:
+				await ctx.channel.send('Took too long to answer!')
+			else:
+				try:
+					channel = await cat_guild.create_text_channel(name=channel.content)
+					self.db.cur.execute("INSERT INTO guild VALUES(?, ?, ?, ?)", (guildID, id, channel.id, cat_id.content))
+					self.db.con.commit()
+				except:
+					await ctx.channel.send("You didn't enter the names properly.\nUse `!setup` again!")
+				else:
+					messages = await ctx.channel.history(limit=6).flatten()
+					for each_message in messages:
+						await each_message.delete()
+					await ctx.channel.send(f"** Your created text Channel `{channel}` with success**")
+
+
+def setup(bot):
+	db = DaoFactory()
+	bot.add_cog(Search(bot, db))
---
+++ src/dao/classifier.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+class DaoFactory:
+	def __init__(self):
+		print("Init Database ...")
+		self.con = sqlite3.connect('finder.db')
+		self.cur = self.con.cursor()
+		self.cur.execute('''SELECT count(*) from sqlite_master WHERE type='table' AND name='expeditions' ''')
+		rows = self.cur.fetchall()
+		if rows[0][0] == 0:
+			self.__init_tables_expeditions()
+		print("Init Database ok")
+
+
+	def __init_tables_expeditions(self):
+		self.cur.execute('''CREATE TABLE IF NOT EXISTS expeditions
+               (name text, level int, number_p int, territory text)''')
+		self.cur.execute('''CREATE TABLE IF NOT EXISTS guild
+               (guildID text, id text, channelID text, categoryID text)''')
+		# Init Expeditions
+		self.cur.execute('''INSERT INTO expeditions VALUES('Amrine Excavation', 25, 5, 'Windsward')''')
+		self.cur.execute('''INSERT INTO expeditions VALUES('Starstone Barrows', 35, 5, 'Everfall')''')
+		self.cur.execute('''INSERT INTO expeditions VALUES('The Depths', 45, 5, 'Restless Shore')''')
+		self.cur.execute('''INSERT INTO expeditions VALUES('Dynasty Shipyard', 55, 5, 'Ebonscale Reach')''')
+		self.cur.execute('''INSERT INTO expeditions VALUES('Garden of Genesis', 60, 5, 'Edengrove')''')
+		self.cur.execute('''INSERT INTO expeditions VALUES('Lazarus Instrumentality', 60, 5, 'Reekwater')''')
+		self.con.commit()
+
+	# def __del__(self):
+	# 	self.cur.close()
